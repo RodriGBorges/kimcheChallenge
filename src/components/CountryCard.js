1a3beab2ad81673d14fdaa1fddb4bcdb6867b535
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-05-14 14:05:48 -0300

    Estructura inicial

---
+++ src/components/CountryCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+// Hago las tarjetas de los países por separado para no cargar tanto el home
+export const CountryCard = () => {
+    return (
+        <div className='countryCard-container'>
+            <article className='card'>
+                <div className='head'>
+                    <small>Ar</small>
+                    <p>Argentina</p>
+                    <i class="fas fa-flag"></i>
+                </div>
+                <div className='info'>
+                    <p>Capital: Buenos Aires</p>
+                    <p>Phone: 54</p>
+                    <p>Currency: ARS</p>
+                    <small>more</small>
+                </div>
+            </article>
+            <article className='card'>
+                <div className='head'>
+                    <small>Ar</small>
+                    <p>Argentina</p>
+                    <i class="fas fa-flag"></i>
+                </div>
+                <div className='info'>
+                    <p>Capital: Buenos Aires</p>
+                    <p>Phone: 54</p>
+                    <p>Currency: ARS</p>
+                    <small>more</small>
+                </div>
+            </article>
+            <article className='card'>
+                <div className='head'>
+                    <small>Ar</small>
+                    <p>Argentina</p>
+                    <i class="fas fa-flag"></i>
+                </div>
+                <div className='info'>
+                    <p>Capital: Buenos Aires</p>
+                    <p>Phone: 54</p>
+                    <p>Currency: ARS</p>
+                    <small>more</small>
+                </div>
+            </article>
+            <article className='card'>
+                <div className='head'>
+                    <small>Ar</small>
+                    <p>Argentina</p>
+                    <i class="fas fa-flag"></i>
+                </div>
+                <div className='info'>
+                    <p>Capital: Buenos Aires</p>
+                    <p>Phone: 54</p>
+                    <p>Currency: ARS</p>
+                    <small>more</small>
+                </div>
+            </article>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { CountryCard } from '../components/CountryCard';
 
 export const Home = () => {
 
@@ -38,8 +39,50 @@ export const Home = () => {
     })
 
     return (
-        <div>
-            <h1>Kimche Challenge</h1>
+        <div className='home'>
+            <h1 className='header'>Country Search</h1>
+            <h4>Enter the first letters of the country. You can group by continent or language.</h4>
+            <div className='searchContainer'>
+                <form>
+                    <input  type="text" className='search' placeholder="Chi..."/>
+                    <button>
+                        <i className='icon-search fas fa-search'></i>
+                    </button>
+                </form>
+                <div>
+                    <button>Continent</button>
+                    <button>Language</button>
+                </div>
+            </div>
+            <section>
+                <h2>Asia</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>Africa</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>Europe</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>North America</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>South America</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>Oceania</h2>
+                <CountryCard />
+            </section>
+            <section>
+                <h2>Antarctica</h2>
+                <CountryCard />
+            </section>
+            <h2 className='footer'>Kimche Challenge</h2>
         </div>
     )
 }
